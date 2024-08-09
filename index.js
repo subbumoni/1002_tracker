10037cb78c297692414d516d12ca74fd9dfcc41f
subbumoni <subramanianbeee@gmail.com>
2024-08-09 13:23:13 +0530

    Application Server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const http_server = express();
+
+const PORT = 1000;
+const HOSTNAME = "localhost";
+
+http_server.listen(PORT, HOSTNAME, () => {
+  console.log(`server started at http://${HOSTNAME}:${PORT}`);
+});
+
+
+http_server.get('/', (req, res) => {
+    res.status(200).json({
+    message:"server started successfully",
+    })
+})
